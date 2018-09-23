12743e17388e551238bec7663591351e2fd81b1b
S.Mojtaba Hosseini <s.mojtaba1987@gmail.com>
2018-09-23 16:36:02 +0330

    23.9.2018.3

--- DatingApp.API/Dtos/UserForRegisterDtos.cs
+++ DatingApp.API/Dtos/UserForRegisterDtos.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDtos
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength = 4, ErrorMessage = "You Must Specify Password Between 4 and 8")]
         public string Password { get; set; }
     }
 }
